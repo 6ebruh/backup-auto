38aadd8c09d3f00c753e81d69adc0d76f5fd8bda
6ebruh <108336499+6ebruh@users.noreply.github.com>
2024-11-27 00:01:32 +0300

    add incognito

--- Browser.py
+++ Browser.py
@@ -4,7 +4,9 @@ from selenium.webdriver.common.by import By
 class Browser():
 
     def __init__(self):
-        self.driver = webdriver.Chrome()
+        self.chrome_options = webdriver.ChromeOptions()
+        self.chrome_options.add_argument("--incognito")
+        self.driver = webdriver.Chrome(self.chrome_options)
         self.driver.maximize_window()
 
     def open_link(self, link):
