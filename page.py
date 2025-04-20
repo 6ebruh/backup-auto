72315d336ec691f033eeee9bc78ec9e2d292370a
matthew <kmm.21@uni-dubna.ru>
2025-04-21 01:10:09 +0300

    xpaths

---
+++ page.py
@@ -0,0 +1,20 @@
+
+#mail
+mail_enter = "//*[@class='ph-login svelte-ttryjx']"
+mail_login_input = "//*[@name='username']"
+mail_enter_button = "//span[contains(text(), 'Войти')]"
+skip_button = "//span[contains(text(), 'Войти другим способом')]"
+mail_password_input = "//*[@placeholder='Пароль']"
+mail_login_frame = "//*[@class='ag-popup__frame__layout__iframe']"
+mail_folder = "//div[contains(text(), 'Backup')]"
+mail_profile_button = "//img[@alt='rinkachirukuki1@mail.ru']"
+mail_exit_button = "//div[contains(text(), 'Выйти')]"
+
+#owa
+login_label = "//*[@id='username']"
+password_label = "//*[@id='password']"
+enter_button = "//*[@id='lgnDiv']/div[9]/div/span"
+profile_button = "//button[@aria-label='Меню Техподдержка Мониторинга ДИТ с подменю']"
+exit_button = "//span[contains(text(), 'Выйти')]"
+folder = "//span[contains(text(), 'Backup СМИС')]"
+table_of_vms = "//*[@id='x_y711']/tbody/tr/td/div/table"
